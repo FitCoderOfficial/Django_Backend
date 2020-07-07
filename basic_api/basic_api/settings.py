9845d0e1f5c4fb95a61e5fafa6d32df54356c504
Seabyeok-Shin <toqur1219@naver.com>
2020-07-07 13:08:30 +0900

    feat:django allauth

---
+++ basic_api/basic_api/settings.py
@@ -0,0 +1,147 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '((@6shs^p9tdvaagc)$ghipdvz+xfmfz@xbjl$x7p6npzz-7#r'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+
+    'core',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'basic_api.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'basic_api.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+#DTABASES = {
+        #        'default' : {
+        #            'ENGINE' : 'django.db.backends.sqlite3',
+        #            'NAME': 'Backend_DB'
+        #
+        #            }
+        #        }
+
+DATABASES = {
+       'default': {
+               'ENGINE': 'django.db.backends.postgresql_psycopg2',
+               #"ENGINE": "django.db.backends.postgresql",
+               'NAME': 'restapi',
+               'USER': 'master',
+               'PASSWORD': 'toqur9393!!',
+               'HOST':'localhost',
+               'PORT': '5432',
+         }
+ }
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = 'ko-kr'
+
+TIME_ZONE = 'Asia/Seoul'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ["backend/Django_Backend/basic_api/static"] 
+
+
+EST_FRAMEWORK = {
+        'DEFAULT_PERMISSION_CLASSES': [
+                                       'rest_framework.authentication.TokenAuthentication' 
+                                       #'rest_framework.permissions.IsAuthenticated',
+                                       #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+                                ]
+}
+
+SITE_ID = 1
+
+REST_USE_JWT = True
+
+
